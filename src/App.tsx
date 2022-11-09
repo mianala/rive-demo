2892f728258c48b581e1fa99c2f58288a2a17cb3
mianala <mianalaloharano@gmail.com>
2022-11-09 07:37:34 +0300

    up

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Rive from "@rive-app/react-canvas"
+
+const App = () => (
+  <Rive
+    style={{ width: "100vw", height: "100vh" }}
+    src="https://cdn.rive.app/animations/vehicles.riv"
+  />
+)
+
+export default App
